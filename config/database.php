35d73d80b6e99c57b17c079b4b0f25dace9fcec3
Nazheef Thareq Asywal <nazheefthareq15@gmail.com>
2025-05-18 17:02:50 +0700

    add: koneksi database

---
+++ config/database.php
@@ -0,0 +1,18 @@
+<?
+    // fungsi untuk koneksi ke database
+    function connectDB() {
+        $host = 'localhost';
+        $dbname = 'db_asriraya';
+        $username = 'root';
+        $password = '';
+
+        // error handling menggunakan PDO
+        try {
+            $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $error) {
+            die("Koneksi Database Gagal: ".$error->getMessage());
+        }
+    }
+?>
