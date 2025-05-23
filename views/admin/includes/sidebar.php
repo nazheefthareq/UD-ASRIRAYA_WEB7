faf105cb99c300e0a1ee9c98c85308fb0f0c9f8d
Nazheef Thareq Asywal <nazheefthareq15@gmail.com>
2025-05-23 15:55:31 +0700

    add: includes file separation for admin header, footer, and sidebar

---
+++ views/admin/includes/sidebar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- lineicons CDN -->
+    <link rel="stylesheet" href="https://cdn.lineicons.com/5.0/lineicons.css" />
+    <!-- Bootstrap 5 CSS CDN & CSS Styling-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/styles.css">
+    <!-- Bootstrap 5 JS CDN & JS Script -->
+    <script defer src="<?= BASE_URL ?>assets/js/script.css"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="wrapper">
+        <aside id="sidebar">
+            <div class="d-flex">
+                <button id="toggle-btn" type="button">
+
+                </button>
+            </div>
+        </aside>
+    </div>
+</body>
+
+</html>
