0b70937d14b5174b5429c2136d6a257c31a6e0bb
Nazheef Thareq <nazheefthareq15@gmail.com>
2025-05-30 14:19:01 +0700

    format: file formatting

--- views/admin/laporan/laporan_pemasukan.php
+++ views/admin/laporan/laporan_pemasukan.php
@@ -1,11 +1,12 @@
 <?php
-    require_once __DIR__ . '/../../../models/laporan.php';
-    $laporanModel = new Laporan();
-    $laporanList = $laporanModel->getAllLaporan();
+require_once __DIR__ . '/../../../models/laporan.php';
+$laporanModel = new Laporan();
+$laporanList = $laporanModel->getAllLaporan();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Stok Barang</title>
@@ -13,27 +14,33 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap');
+
         body {
             background-color: #F4F6FF;
             font-family: Plus Jakarta Sans;
         }
-        .sidebar .logo{
+
+        .sidebar .logo {
             color: #F3C623;
         }
+
         .sidebar {
             height: 100vh;
             background-color: #10375C;
             color: #fff;
         }
+
         .sidebar a {
             color: #fff;
             text-decoration: none;
             display: block;
             padding: 12px 20px;
         }
+
         .sidebar a:hover {
             color: #F3C623;
         }
+
         .info-card {
             background-color: #10375C;
             color: #fff;
@@ -41,32 +48,36 @@
             padding: 20px;
             text-align: center;
         }
+
         .info-card h3 {
             font-weight: bold;
         }
-        .table thead th{
+
+        .table thead th {
             background-color: #10375C;
             color: #F3C623;
         }
     </style>
 </head>
-<body>
-<div class="container-fluid">
-    <div class="row">
-        <!-- Sidebar -->
-        <?php include "../includes/sidebar.php" ?>
-        <!-- Main Content -->
-        <div class="col-md-10 p-4">
-            <h2 class="mb-4 fw-bold">Laporan Pemasukan</h2>
 
-            <!-- Informasi Kartu -->
-            <div class="row mb-4">
-                <div class="col-md-4">
-                    <div class="info-card">
-                        <h3 style="color: #F3C623">278.456.200</h3>
-                        <p>Total Pemasukan</p>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Sidebar -->
+            <?php include "../includes/sidebar.php" ?>
+            <!-- Main Content -->
+            <div class="col-md-10 p-4">
+                <h2 class="mb-4 fw-bold">Laporan Pemasukan</h2>
+
+                <!-- Informasi Kartu -->
+                <div class="row mb-4">
+                    <div class="col-md-4">
+                        <div class="info-card">
+                            <h3 style="color: #F3C623">278.456.200</h3>
+                            <p>Total Pemasukan</p>
+                        </div>
                     </div>
-                </div>
 
 </body>
+
 </html>
