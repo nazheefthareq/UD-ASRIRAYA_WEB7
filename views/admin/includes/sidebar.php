b26d5a76992dbc93c0ee9fa56b08f36ca1dcf84b
Muhamad Wildan Razzan <wildanrazzan82@gmail.com>
2025-05-30 13:13:29 +0700

    change: laporan_pemasukan directory and sidebar location link

--- views/admin/includes/sidebar.php
+++ views/admin/includes/sidebar.php
@@ -2,7 +2,7 @@
     <div class="logo p-3 fw-bold fs-5 d-flex gap-3"><img src="../../../assets/img/logo.png" alt="logo" width="35" height="35">Asri Raya Admin</div>
     <a href="#" class="d-flex gap-2"><i class="bi bi-grid"></i>Dashboard</a>
     <a href="#" class="d-flex gap-2"><i class="bi bi-inbox-fill"></i>Kasir</a>
-    <a href="../manajemen_stok/manajemen_stok.php" class="d-flex gap-2" style="color: #F3C623"><i class="bi bi-archive"></i>Stok Barang</a>
-    <a href="../laporan_pemasukan.php" class="d-flex gap-2"><i class="bi bi-clipboard2-data"></i>Laporan Keuangan</a>
+    <a href="../manajemen_stok/manajemen_stok.php" class="d-flex gap-2"><i class="bi bi-archive"></i>Stok Barang</a>
+    <a href="../laporan/laporan_pemasukan.php" class="d-flex gap-2"><i class="bi bi-clipboard2-data"></i>Laporan Keuangan</a>
     <a href="#" class="d-flex gap-2"><i class="bi bi-journal-text"></i>Manajemen Artikel</a>
 </aside>
