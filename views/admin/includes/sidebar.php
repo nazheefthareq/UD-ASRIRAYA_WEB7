d7852244b6fb98e83f43631c15eac51e1ab92190
Noval Aditya Candra Pratama <143941153+NovalAdityaCP@users.noreply.github.com>
2025-05-31 10:07:57 +0700

    Update sidebar.php

--- views/admin/includes/sidebar.php
+++ views/admin/includes/sidebar.php
@@ -1,6 +1,6 @@
 <aside class="col-md-2 sidebar p-0">
     <div class="logo p-3 fw-bold fs-5 d-flex gap-3"><img src="../../../assets/img/logo.png" alt="logo" width="35" height="35">Asri Raya Admin</div>
-    <a href="#" class="d-flex gap-2"><i class="bi bi-grid"></i>Dashboard</a>
+    <a href="../dashboard.php" class="d-flex gap-2"><i class="bi bi-grid"></i>Dashboard</a>
     <a href="../kasir/kasir.php" class="d-flex gap-2"><i class="bi bi-inbox-fill"></i>Kasir</a>
     <a href="../manajemen_stok/manajemen_stok.php" class="d-flex gap-2"><i class="bi bi-archive"></i>Stok Barang</a>
     <a href="../laporan/laporan_pemasukan.php" class="d-flex gap-2"><i class="bi bi-clipboard2-data"></i>Laporan Keuangan</a>
