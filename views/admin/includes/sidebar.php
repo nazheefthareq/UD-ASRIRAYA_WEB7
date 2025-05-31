a6e5650cafbbb8f45597081032e52265557d66b9
Noval Aditya Candra Pratama <143941153+NovalAdityaCP@users.noreply.github.com>
2025-05-31 10:22:52 +0700

    +tombol logout

--- views/admin/includes/sidebar.php
+++ views/admin/includes/sidebar.php
@@ -1,8 +1,17 @@
-<aside class="col-md-2 sidebar p-0">
-    <div class="logo p-3 fw-bold fs-5 d-flex gap-3"><img src="../../../assets/img/logo.png" alt="logo" width="35" height="35">Asri Raya Admin</div>
-    <a href="../dashboard.php" class="d-flex gap-2"><i class="bi bi-grid"></i>Dashboard</a>
-    <a href="../kasir/kasir.php" class="d-flex gap-2"><i class="bi bi-inbox-fill"></i>Kasir</a>
-    <a href="../manajemen_stok/manajemen_stok.php" class="d-flex gap-2"><i class="bi bi-archive"></i>Stok Barang</a>
-    <a href="../laporan/laporan_pemasukan.php" class="d-flex gap-2"><i class="bi bi-clipboard2-data"></i>Laporan Keuangan</a>
-    <a href="#" class="d-flex gap-2"><i class="bi bi-journal-text"></i>Manajemen Artikel</a>
+<aside class="col-md-2 sidebar p-0 d-flex flex-column" style="height: 100vh;">
+    <div class="logo p-3 fw-bold fs-5 d-flex gap-3">
+        <img src="../../../assets/img/logo.png" alt="logo" width="35" height="35">
+        Asri Raya Admin
+    </div>
+
+    <a href="../dashboard.php" class="d-flex gap-2 p-2"><i class="bi bi-grid"></i>Dashboard</a>
+    <a href="../kasir/kasir.php" class="d-flex gap-2 p-2"><i class="bi bi-inbox-fill"></i>Kasir</a>
+    <a href="../manajemen_stok/manajemen_stok.php" class="d-flex gap-2 p-2"><i class="bi bi-archive"></i>Stok Barang</a>
+    <a href="../laporan/laporan_pemasukan.php" class="d-flex gap-2 p-2"><i class="bi bi-clipboard2-data"></i>Laporan Keuangan</a>
+    <a href="#" class="d-flex gap-2 p-2"><i class="bi bi-journal-text"></i>Manajemen Artikel</a>
+
+    <div style="flex-grow: 1;"></div>
+    <a href="/UD-ASRIRAYA_WEB7-main/logout.php" class="d-flex gap-2 p-2 mb-4">
+        <i class="bi bi-box-arrow-right"></i>Logout
+    </a>
 </aside>
