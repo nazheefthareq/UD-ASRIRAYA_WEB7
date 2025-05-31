b6a814915c097cfdb7c2bcbefcf6dd9cccb54b8b
Muhamad Wildan Razzan <wildanrazzan82@gmail.com>
2025-05-31 07:19:10 +0700

    fix: controller for kasir

--- controllers/kasirController.php
+++ controllers/kasirController.php
@@ -1,33 +1,45 @@
 <?php
-require_once __DIR__ . '/../config/database.php';
+session_start();
+require_once __DIR__ . '/../models/produk.php';
+require_once __DIR__ . '/../models/kasir.php';
 
-class Laporan {
-    private $conn;
+$produkModel = new Produk();
+$transaksiModel = new Kasir();
 
-    public function __construct() {
-        $conn = connectDB();
-        $this->conn = $conn;
+if (!isset($_SESSION['keranjang'])) $_SESSION['keranjang'] = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id_produk'];
+    $jumlah = intval($_POST['jumlah'] ?? 1);
+
+    if (isset($_POST['tambah_keranjang'])) {
+        $produk = $produkModel->getProdukID($id);
+        if ($jumlah > 0 && $jumlah <= $produk['stok_produk']) {
+            $_SESSION['keranjang'][$id] = ($_SESSION['keranjang'][$id] ?? 0) + $jumlah;
+        }
     }
 
-    public function getAllLaporan() {
-        $sql = "SELECT * FROM transaksi_kasir ORDER BY tanggal_transaksi DESC";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if (isset($_POST['ubah_jumlah']) && $jumlah > 0) {
+        $_SESSION['keranjang'][$id] = $jumlah;
     }
 
-    public function getLaporanByTanggal($dari, $sampai) {
-        $sql = "SELECT * FROM transaksi_kasir 
-                WHERE tanggal_transaksi BETWEEN :dari AND :sampai
-                ORDER BY tanggal_transaksi DESC";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bindParam(':dari', $dari);
-        $stmt->bindParam(':sampai', $sampai);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if (isset($_POST['hapus_item'])) {
+        unset($_SESSION['keranjang'][$id]);
     }
 
-    
+    if (isset($_POST['checkout'])) {
+        foreach ($_SESSION['keranjang'] as $id => $jumlah) {
+            $produk = $produkModel->getProdukID($id);
+            $harga = $produk['harga_jual'];
+            $total = $harga * $jumlah;
+            $transaksiModel->simpanTransaksi($id, $jumlah, $harga, $total);
+            $produkModel->kurangiStok($id, $jumlah);
+        }
+        $_SESSION['keranjang'] = [];
+    }
+
+    header("Location: ../views/admin/kasir/kasir.php"); // Hindari resubmission
+    exit();
 }
 
 ?>
